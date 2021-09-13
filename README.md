c0e6fbe7c0ae7867cb89c6f9eab74e785e4179ef
Ram Bhardwaj <63110845+programmersidharth@users.noreply.github.com>
2021-09-13 18:40:14 +0530

    added projects with releases

---
+++ README.md
@@ -0,0 +1,7 @@
+# Welcome to GiPI!
+
+The **Gideon Package Index (GiPI)** is a repository of software for the Gideon Skills.
+
+**GiPI** helps you find and install software developed and shared by the Gideon community.  [Learn about installing packages](https://mydomain.org/installing/ "External link").
+
+Package authors use **GiPI** to distribute their software.  [Learn how to package your Gideon code for GiPI](https://mydomain.org/tutorials/packaging-projects/ "External link").
---
+++ projects/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.conf import settings
+
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT / user_<id>/<filename>
+    # instance is the self Release model, I can access the Release model by it!
+    return 'packages/user_{0}/{1}/release_{2}/{3}'.format(instance.author.id, instance.project.id, instance.id, filename)
+
+class Project(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
+    description = models.TextField()
+    url = models.URLField()
+    requirements = models.TextField()
+    released_on = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+
+class Release(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="releases")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=100)
+    file = models.FileField(upload_to=user_directory_path)
+    released_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['project', 'name'], name='project_release_name_unique')
+        ]
+    def __str__(self):
+        return f"release {self.name} in project {self.project} id={self.id}"
